24410d221788980c331cb5b6dc47c8d043f761cd
Olsahaliti <olsahaliti931@gmail.com>
2024-11-21 09:48:47 -0800

    punova nje kalkulator

---
+++ kalkulatori.js
@@ -0,0 +1,23 @@
+ let numri1 = +prompt("Shkruaj numrin e pare")
+let operatori = prompt("Shkruaj operatorin")
+let numri2 = +prompt("Shkruaj numrin e dyte")
+
+
+console.log(numri1)
+console.log(operatori)
+console.log(numri2)
+
+
+if(operatori== '+'){ 
+  alert( numri1 + numri2)
+}
+if(operatori== "-"){ 
+    alert( numri1 - numri2)
+}
+if(operatori== "*"){ 
+    alert( numri1 * numri2)
+}
+if(operatori== "/"){ 
+    alert( numri1 / numri2)
+}
+
---
+++ script.js
@@ -0,0 +1,22 @@
+//let x = 4 + 4;
+//operator unar
+//let numri = -4;
+//console.log(numri1);
+//e shendrron vleren ne number
+//let numri2 = +"4";
+//let numri3 = +false;
+//let numri = 0
+//inkrement rritje
+//console.log("Rresht nr 10", ++numri);
+//console.log("Rresht nr 11", numri++);
+//console.log("Rresht nr 12", numri);
+//dekrement zbritje
+//console.log("Rreshti nr 14", --numri);
+//console.log("Rreshti nr 15", numri--);
+//console.log("Rreshti nr 16",  numri);
+
+let bora = confirm("a po bjen bore");
+
+if (bora) { 
+    alert("Olsa rri ne shpi");
+}
